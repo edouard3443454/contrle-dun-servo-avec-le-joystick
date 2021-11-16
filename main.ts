549b514773f712ab7e86b2b19f40231ae1d6eb70
edouard3443454 <93147216+edouard3443454@users.noreply.github.com>
2021-11-16 10:13:26 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,5 @@
 basic.forever(function () {
-    basic.clearScreen()
-    led.plot(pins.map(
-    pins.analogReadPin(AnalogPin.P0),
-    0,
-    1023,
-    10,
-    170
-    ), pins.map(
+    servos.P0.setAngle(pins.map(
     pins.analogReadPin(AnalogPin.P1),
     0,
     1023,
